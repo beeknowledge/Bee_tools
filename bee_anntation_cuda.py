baf0f0cc26c04d6353fcaab243e93d8da1c69e45
BeeKnowledge <70070814+takashide@users.noreply.github.com>
2021-07-06 11:14:25 +0900

    Add files via upload

--- bee_anntation_cuda.py
+++ bee_anntation_cuda.py
@@ -1,6 +1,8 @@
 import cv2
 import time
 import csv
+import glob
+import os
 
 print(cv2.__file__) 
 CONFIDENCE_THRESHOLD = 0.25
@@ -13,6 +15,8 @@ class_names = []
 with open("coco.names", "r") as f:
     class_names = [cname.strip() for cname in f.readlines()]
 
+files =glob.glob('/home/bee/work/temp/org/*[jJ][pP][gG]')
+path = '/home/bee/work/temp/make'
 
 
 #net = cv2.dnn.readNet("yolov3-tiny.weights", "yolov3-tiny.cfg")
@@ -29,45 +33,46 @@ model = cv2.dnn_DetectionModel(net)
 
 model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
 
-img = cv2.imread("test.jpg")
-img_base = img#ベース画像
-v_height, v_width, _channels = img.shape
+for fname in files: 
+    img = cv2.imread(fname, cv2.IMREAD_COLOR)
+    img_base = img
+    v_height, v_width, _channels = img.shape
+    name = os.path.splitext(os.path.basename(fname))[0]
+    classes, scores, boxes = model.detect(img, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
 
-
-classes, scores, boxes = model.detect(img, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
-
-annotation = []                    
-for (classid, score, box) in zip(classes, scores, boxes):
-    color = COLORS[int(classid) % len(COLORS)]
-    label = "%s : %f" % (class_names[classid[0]], score)
-    print(box)
+    annotation = []                    
+    for (classid, score, box) in zip(classes, scores, boxes):
+        color = COLORS[int(classid) % len(COLORS)]
+        label = "%s : %f" % (class_names[classid[0]], score)
+        print(box)
     
-    x, y, w, h = box
-    cv2.rectangle(img, (x, y, w, h), color, 2)
+        x, y, w, h = box
+        cv2.rectangle(img, (x, y, w, h), color, 2)
 
-    center_x= round((w/2 + x)/v_width, 6)
-    center_y = round((h/2 + y)/v_height, 6)
-    print((w/2 + x),(h/2 + y ))
+        center_x= round((w/2 + x)/v_width, 6)
+        center_y = round((h/2 + y)/v_height, 6)
+        print((w/2 + x),(h/2 + y ))
     
 
-    y_width = round((w)/v_width, 6)
-    y_height = round(h/v_height, 6)
-    print(w,h)
+        y_width = round((w)/v_width, 6)
+        y_height = round(h/v_height, 6)
+        print(w,h)
     
-    classCounter = str(classid[0])
-    hhh = str(classid[0]) + ' ' + str(center_x) + ' ' + str(center_y) + ' ' + str(y_width) + ' ' + str(y_height)
-    cv2.putText(img, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
-    annotation.append([hhh])
-    print(hhh)
+        classCounter = str(classid[0])
+        hhh = str(classid[0]) + ' ' + str(center_x) + ' ' + str(center_y) + ' ' + str(y_width) + ' ' + str(y_height)
+        cv2.putText(img, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+        annotation.append([hhh])
+        print(hhh)
 
 
-with open('test.txt', 'w') as fout:
-    writer = csv.writer(fout)
-    writer.writerows(annotation)
-    fout.close
+    with open(path + '/' + name  + '.txt', 'w') as fout:
+        writer = csv.writer(fout)
+        writer.writerows(annotation)
+        fout.close
+    cv2.imwrite(os.path.join(path , name + 'test.jpg'), img)
 
 
-cv2.imwrite("out.jpg",img)
+
 cv2.imshow("detections", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
